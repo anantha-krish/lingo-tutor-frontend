f0b4688333c0d157f8fdd963daa82e8607c00c82
Anantha Krishnan <anantha.krishnan@outlook.com>
2024-04-25 12:50:14 +0530

    Remove react bootstrap error

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,23 +1,23 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
-import logo from './images/logo.jpeg';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Formik } from "formik";
+import logo from "./images/logo.jpeg";
 import * as Yup from "yup";
 
 const validationSchema = Yup.object().shape({
-  username: Yup.string().required('Username is required'),
+  username: Yup.string().required("Username is required"),
   password: Yup.string()
-    .required('Password is required')
-    .min(8, 'Password must be at least 8 characters')
+    .required("Password is required")
+    .min(8, "Password must be at least 8 characters")
     .matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/,
-          'Password must contain at least one uppercase letter, one lowercase letter, one digit and one special character'
+      "Password must contain at least one uppercase letter, one lowercase letter, one digit and one special character"
     ),
 });
 
 export const LoginUser = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const handleUsernameChange = (event) => {
@@ -36,65 +36,80 @@ export const LoginUser = () => {
         password: password,
       });
       // Your login logic here
-      console.log('Login successful');
+      console.log("Login successful");
     } catch (error) {
       // Handle validation errors
-      console.error('Validation Error:', error.message);
+      console.error("Validation Error:", error.message);
     }
   };
 
   const handleRegister = () => {
-    navigate('/register');
-    console.log('Redirecting to registration page');
+    navigate("/register");
+    console.log("Redirecting to registration page");
   };
 
   return (
-    <Container>
-      <Row className="justify-content-center mt-5">
-        <Col xs={12} sm={8} md={6}>
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <div className="col col-xs-12 col-sm-8 col-md-6">
           <div className="text-center mb-4">
-            <img src={logo} alt="Logo" style={{ maxWidth: '100%', maxHeight: '150px' }} />
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ maxWidth: "100%", maxHeight: "150px" }}
+            />
           </div>
-          <Form noValidate validated={validationSchema}>
-            <Form.Group controlId="formBasicUsername">
-              <Form.Control
-                type="text"
-                placeholder="Username"
-                value={username}
-                //isValid={LoginSchema}
-                onChange={handleUsernameChange}
-                required
-              />
-               <Form.Control.Feedback type="invalid">
-                Please provide a valid username
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-              <Form.Control
-                type="password"
-                placeholder="Password"
-                value={password}
-                //isValid={LoginSchema}
-                onChange={handlePasswordChange}
-                required
-              />
-              <Form.Control.Feedback type="invalid">
-                Please provide a valid password
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Button variant="primary" className="btn-block" onClick={handleLogin}>
-              Login
-            </Button>
-
-            <Button variant="secondary" className="btn-block mt-3" onClick={handleRegister}>
-              Register
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+          <Formik
+            initialValues={{
+              username: "",
+              password: "",
+            }}
+            validationSchema={validationSchema}
+            onSubmit={() => {}}
+          >
+            {(props) => (
+              <>
+                {" "}
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  className={
+                    props.touched.firstName && props.errors.firstName
+                      ? "is-invalid"
+                      : ""
+                  }
+                  //isValid={LoginSchema}
+                  onChange={handleUsernameChange}
+                  required
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  //isValid={LoginSchema}
+                  onChange={handlePasswordChange}
+                  required
+                />
+                <button
+                  className="btn btn-primary btn-block"
+                  onClick={handleLogin}
+                >
+                  Login
+                </button>
+                <button
+                  className="btn btn-primary btn-block"
+                  onClick={handleRegister}
+                >
+                  Register
+                </button>
+              </>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 // Import our custom CSS
 import "./index.scss";
 // Import all of Bootstrap's JS
+// eslint-disable-next-line no-unused-vars
 import * as bootstrap from "bootstrap";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
