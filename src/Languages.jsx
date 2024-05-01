045707cc0ee131ea13c39cc717e82b1ece3c4f51
Pavana Wilson <pavanawilson@gmail.com>
2024-05-01 21:50:23 +0530

    languagelist update

--- src/Languages.jsx
+++ src/Languages.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { getLanguages } from "./api";
-import { Container, Button, Card } from "react-bootstrap";
+import { Container, Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 
 const Languages = () => {
@@ -19,11 +19,8 @@ const Languages = () => {
   return (
     <Container>
       {languages.map((language) => (
-      <Card key={language.id} style={{ width: '18rem', margin: '10px' }} onClick={() => { navigate(`/languages/${language.id}`); }}>
-        <Card.Img variant="top" src="./assets/images/404.jpeg" />
-        <Card.Body>
-          <Card.Title>{language.name}</Card.Title>
-        </Card.Body>
+      <Card key={language.id} style={{ width: '60rem', margin: '70px', height: '20rem', background:'#cce6ff'}} onClick={() => { navigate(`/languages/${language.id}`); }}>
+          <Card.Title style={{ fontSize: '7rem', textAlign: 'center', margin: '5rem'}}>{language.name}</Card.Title>
       </Card>
       ))}
     </Container>
