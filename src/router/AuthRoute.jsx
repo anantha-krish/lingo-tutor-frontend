917bb98bd4ce49517b8346f15cedcdb0bc67a7a0
Anantha Krishnan <anantha.krishnan@outlook.com>
2024-05-02 23:27:15 +0530

    Add Nav bar

--- src/router/AuthRoute.jsx
+++ src/router/AuthRoute.jsx
@@ -1,121 +1,29 @@
-import { Link, Navigate, Outlet } from "react-router-dom";
-import { isAuthenticated, getUsername } from "../sessionManager";
-import { Container, Navbar, Nav, Image, Row, Col } from "react-bootstrap";
-import { useAxiosLoader } from "../api";
+import { Col, Container, Row } from "react-bootstrap";
+import { Navigate, Outlet } from "react-router-dom";
 import { BarLoader } from "react-spinners";
-import BrandLogo from "../assets/images/logo_portrait.png";
+import { useAxiosLoader } from "../api";
+import { LUINavbar } from "../components";
+import { isAuthenticated } from "../sessionManager";
 export const AuthRoute = () => {
   const [loading] = useAxiosLoader();
-  var username = getUsername();
   return isAuthenticated() ? (
-    <Container fluid>
-      <Navbar>
-        <Row className="w-100">
-          <Col lg={2}>
-            <div className="p-3 bg-white">
-              <Image src={BrandLogo} height={70} style={{ margin: 10 }} />
-            </div>
-          </Col>
-          <Col lg={10}>
-            <Nav className="p-2 bg-primary">
-              <Row className="p-2 bg-primary">
-                <Col>
-                  <Link className="text-white" to="/">
-                    Home
-                  </Link>
-                </Col>
-                <Col>
-                  <Link className="text-white" to="/quizzes/7001">
-                    Quizzes (Dev only)
-                  </Link>
-                </Col>
-                <Col>
-                  <Link className="text-white" to="results/quizzes/7001">
-                    Quizz Result (Dev only)
-                  </Link>
-                </Col>
-                <Col>
-                  <Link className="text-white" to="/languages/1001">
-                    Langugage Detail (Dev only)
-                  </Link>
-                </Col>
-                <Col>
-                  <Link className="text-white" to="/dashboard">
-                    User Dashboard
-                  </Link>
-                </Col>
-                <Col>
-                  <Link className="text-white" to="/dummy">
-                    page Not found (Dev only)
-                  </Link>
-                </Col>
-                <Col>
-                  <Link className="text-white" to="/logout">
-                    Logout (Dev only)
-                  </Link>
-                </Col>
-              </Row>
-            </Nav>
-            {/*<Nav className="p-2 bg-primary">
-              <Link to="/">
-                <Nav.Link className="text-white" style={{ margin: 10 }}>
-                  Home
-                </Nav.Link>
-              </Link>
-              <Link to="/quizzes/7001">
-                <Nav.Link className="text-white" style={{ margin: 10 }}>
-                  Quiz
-                </Nav.Link>
-              </Link>
-              <Link to="results/quizzes/7001">
-                <Nav.Link className="text-white" style={{ margin: 10 }}>
-                  Quiz Result
-                </Nav.Link>
-              </Link>
-              <Link to="/languages/1001">
-                <Nav.Link className="text-white" style={{ margin: 10 }}>
-                  Language Detail
-                </Nav.Link>
-              </Link>
-              <Link to="/dashboard">
-                <Nav.Link className="text-white" style={{ margin: 10 }}>
-                  Dashboard
-                </Nav.Link>
-              </Link>
-              <Link to="/dummy">
-                <Nav.Link className="text-white" style={{ margin: 10 }}>
-                  Page Not found
-                </Nav.Link>
-              </Link>
-              <Link to="/logout">
-                <Nav.Link
-                  className="text-white"
-               
-                  style={{ margin: 10 }}
-                >
-                  Logout
-                </Nav.Link>
-              </Link>
-              <h5 className="text-white" style={{ margin: 10 }}>
-                Hi! {username}
-              </h5>
-  </Nav>*/}
+    <>
+      <LUINavbar />
+      <Container fluid>
+        <Row style={{ minHeight: 10 }}>
+          <Col lg={12}>
+            {loading ? (
+              <BarLoader color="#36d7b7" className="w-100" height={10} />
+            ) : (
+              " "
+            )}
           </Col>
         </Row>
-      </Navbar>
-      <Row style={{ minHeight: 10 }}>
-        <Col lg={12}>
-          {loading ? (
-            <BarLoader color="#36d7b7" className="w-100" height={10} />
-          ) : (
-            " "
-          )}
-        </Col>
-      </Row>
-      <Row>
-        <Outlet />
-      </Row>
-    </Container>
+        <Row>
+          <Outlet />
+        </Row>
+      </Container>
+    </>
   ) : (
     <Navigate to="/login" />
   );
