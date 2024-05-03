2b79cc51ae0ad0c6d63e03290763257eb61e56ee
Anantha Krishnan <anantha.krishnan@outlook.com>
2024-05-03 12:33:46 +0530

    Sidebar changes

--- src/pages/LanguageDetail/SectionArticleSideBar.jsx
+++ src/pages/LanguageDetail/SectionArticleSideBar.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Nav } from "react-bootstrap";
+import { Accordion, Nav } from "react-bootstrap";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import { getLanguageById } from "../../api";
 export const SectionArticleSideBar = () => {
@@ -24,13 +24,22 @@ export const SectionArticleSideBar = () => {
   }, [params.languageId, navigate]);
   return (
     <Nav className="text-bg-light sidebar">
-      <div>
-        <ul>
-          {lang.sections.length > 0 &&
-            lang.sections.map((section) => (
-              <li key={section.id} className="p-2">
-                {section.name}
-                <ul>
+      {lang.sections.length > 0 && (
+        <Accordion
+          className="w-100"
+          defaultActiveKey={lang.sections[0].id}
+          alwaysOpen
+        >
+          {lang.sections.map((section, index) => (
+            <Accordion.Item key={`key_${section.id}`} eventKey={section.id}>
+              <Accordion.Header>
+                <strong>
+                  Section {(index + 1).toString().padStart(2, 0)}:{" "}
+                  {section.name}{" "}
+                </strong>
+              </Accordion.Header>
+              <Accordion.Body>
+                <ul style={{ listStyle: "none" }}>
                   {section.articles.map((article) => (
                     <li key={article.id} className="p-2">
                       <Link
@@ -41,27 +50,31 @@ export const SectionArticleSideBar = () => {
                     </li>
                   ))}
                 </ul>
-              </li>
-            ))}
-        </ul>
-      </div>
-      <div>
-        <ul>
+              </Accordion.Body>
+            </Accordion.Item>
+          ))}
           {lang.quizzes.length > 0 && (
-            <>
-              <h4>Quiz</h4>
-              {lang.quizzes.map((quiz) => (
-                <Link
-                  key={quiz.id}
-                  to={`/languages/${params.languageId}/quizzes/${quiz.id}`}
-                >
-                  <li>{quiz.name} </li>
-                </Link>
-              ))}
-            </>
+            <Accordion.Item eventKey="quiz_section">
+              <Accordion.Header>
+                <strong>Quiz Section</strong>
+              </Accordion.Header>
+              <Accordion.Body>
+                <ul style={{ listStyle: "none" }}>
+                  {lang.quizzes.map((quiz) => (
+                    <li key={quiz.id} className="p-2">
+                      <Link
+                        to={`/languages/${params.languageId}/quizzes/${quiz.id}`}
+                      >
+                        {quiz.name}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </Accordion.Body>
+            </Accordion.Item>
           )}
-        </ul>
-      </div>
+        </Accordion>
+      )}
     </Nav>
   );
 };
